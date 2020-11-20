bbbdeebe9a60870d0ca46eaf5a039e9dac542079
Ahmed Hassan Salem <74409660+TheCoderGit@users.noreply.github.com>
2020-11-20 21:21:53 +0200

    for loop

--- hello.py
+++ hello.py
@@ -1,7 +1,2 @@
-name = "Ahmed" #list of strings
-coordinates = (10.0, 20.0) #tuble
-names = ["Alice", "Bob", "Charlie"] #list
-
-print(name[0])
-print(coordinates[0])
-print(names[0])
+for i in range(5):
+    print(i)
